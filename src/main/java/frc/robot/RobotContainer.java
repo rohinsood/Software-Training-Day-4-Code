8752f0366f8a877965b484fe000e8154a8a810cc
rohin <rs.rohinsood@gmail.com>
2022-07-08 02:13:09 -0700

    button bindings & sequential autonomous command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,48 +1,47 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
-import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj.XboxController.Button;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.commands.*;
+import frc.robot.subsystems.Drivetrain;
 
-/**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and button mappings) should be declared here.
- */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+  private final Drivetrain m_drivetrain = new Drivetrain();
+  
+  private final Forward m_forward = new Forward(m_drivetrain);
+  private final Reverse m_reverse = new Reverse(m_drivetrain);
+  private final RightTurn m_rightTurn = new RightTurn(m_drivetrain);
+  private final LeftTurn m_leftTurn = new LeftTurn(m_drivetrain);
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
-    // Configure the button bindings
     configureButtonBindings();
   }
 
-  /**
-   * Use this method to define your button->command mappings. Buttons can be created by
-   * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
-   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
-   */
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+    XboxController Pilot = new XboxController(0);
+    Pilot.getPOV();
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
-  public Command getAutonomousCommand() {
-    // An ExampleCommand will run in autonomous
-    return m_autoCommand;
+    JoystickButton A = new JoystickButton(Pilot, Button.kA.value);
+    JoystickButton B = new JoystickButton(Pilot, Button.kB.value);
+    JoystickButton X = new JoystickButton(Pilot, Button.kX.value);
+    JoystickButton Y = new JoystickButton(Pilot, Button.kY.value);
+
+    m_drivetrain.setDefaultCommand( (new TankDrive(m_drivetrain, Pilot::getLeftY, Pilot::getRightY)) );
+
+    A.whenHeld(m_reverse);
+    B.whenHeld(m_leftTurn);
+    X.whenHeld(m_rightTurn);
+    Y.whenHeld(m_forward);
   }
+
+  public Command getAutonomousCommand() {
+    return new SequentialCommandGroup(
+      m_forward, m_rightTurn
+    );
+  }
+
 }
