1dc25b3db3a9b2ed38e03634b56d7e93aa3b0be1
rohin <rs.rohinsood@gmail.com>
2022-07-08 02:13:46 -0700

    all the other drive commands

---
+++ src/main/java/frc/robot/commands/RightTurn.java
@@ -0,0 +1,38 @@
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Drivetrain;
+
+public class RightTurn extends CommandBase {
+
+    private final Drivetrain m_drivetrain;
+
+    public RightTurn(Drivetrain drivetrain) {
+        m_drivetrain = drivetrain;
+        addRequirements(drivetrain);
+    }
+
+    @Override
+    public void initialize() {
+        m_drivetrain.tankDrive(0.0, 0.0);
+    }
+
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+        m_drivetrain.tankDrive(-0.5, 0.5);
+    }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+        m_drivetrain.tankDrive(0.0, 0.0);
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
